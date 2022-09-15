65011cbdd28402386e656f9631198604ef56f7fb
Islam Naasani <islamnaasani@gmail.com>
2022-09-15 21:41:11 +0300

    close menu on link click(mobile)

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,5 +1,7 @@
 const body = document.body;
 const toggleThemeCheckbox = document.querySelector(".toggle-theme-checkbox");
+const headerMenu = document.querySelector("#menu");
+const headerMenuCheckBox = document.querySelector("#menuToggle>input") as HTMLInputElement;
 const preferenceQuery = window.matchMedia("(prefers-color-scheme: dark)");
 const addDarkMode = () => {
     body.classList.remove("light-mode");
@@ -11,6 +13,12 @@ const addLightMode = () => {
     body.classList.add("light-mode");
 };
 
+headerMenu.addEventListener("click", (e) => {
+    const el = e.target as HTMLElement;
+    if (el.tagName === "A") {
+        headerMenuCheckBox.checked = false;
+    }
+});
 const toggleTheme = () => (!body.classList.contains("dark-mode") ? addDarkMode() : addLightMode());
 
 const checkPreference = () => (preferenceQuery.matches ? addDarkMode() : addLightMode());
