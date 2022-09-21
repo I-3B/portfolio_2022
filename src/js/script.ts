b1d157478f281be5427a461142d8f9db81bd0c3c
Islam Naasani <islamnaasani@gmail.com>
2022-09-21 23:15:06 +0300

    add image loading (need experimenting)

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,16 +1,25 @@
 const body = document.body;
-const toggleThemeCheckbox = document.querySelector(".toggle-theme-checkbox");
+const toggleThemeCheckbox = document.querySelector(".toggle-theme-checkbox") as HTMLInputElement;
 const headerMenu = document.querySelector("#menu");
 const headerMenuCheckBox = document.querySelector("#menuToggle>input") as HTMLInputElement;
 const preferenceQuery = window.matchMedia("(prefers-color-scheme: dark)");
+const themeLS = localStorage.getItem("theme");
+const images = document.querySelectorAll("img") as NodeListOf<HTMLImageElement>;
+images.forEach((img) => {
+    const src = img.src;
+    img.src = "/assets/images/loading.svg";
+    img.onload = () => (img.src = src);
+});
 const addDarkMode = () => {
     body.classList.remove("light-mode");
     body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
 };
 
 const addLightMode = () => {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
+    localStorage.setItem("theme", "light");
 };
 
 headerMenu.addEventListener("click", (e) => {
@@ -21,7 +30,8 @@ headerMenu.addEventListener("click", (e) => {
 });
 const toggleTheme = () => (!body.classList.contains("dark-mode") ? addDarkMode() : addLightMode());
 
-const checkPreference = () => (preferenceQuery.matches ? addDarkMode() : addLightMode());
+const checkPreference = () =>
+    !themeLS ? (preferenceQuery.matches ? addDarkMode() : addLightMode()) : null;
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -35,3 +45,8 @@ hiddenElements.forEach((e) => {
 toggleThemeCheckbox.addEventListener("change", toggleTheme);
 preferenceQuery.addEventListener("change", checkPreference);
 window.addEventListener("DOMContentLoaded", checkPreference);
+if (themeLS === "dark") {
+    addDarkMode();
+} else if (themeLS === "light") {
+    addLightMode();
+}
